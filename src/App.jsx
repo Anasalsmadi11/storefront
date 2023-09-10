35166ce4e07c313e074d8d81e5bbf7290d43633f
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2023-09-10 18:13:00 +0000

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
 import Header from './Components/Header/Header'
 import Categories from './Components/Categories/Categories'
 import Footer from './Components/Footer/Footer'
@@ -8,7 +6,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  // const [count, setCount] = useState(0)
+
 
   return (
     <>
